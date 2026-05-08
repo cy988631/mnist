ffb8af7f2b67b4297a660cdd637c725feef8f077
cjy <1181869833@qq.com>
2026-05-09 03:11:02 +0800

    dataset

--- dataset.py
+++ dataset.py
@@ -5,12 +5,28 @@ from torchvision import datasets
 from torchvision import transforms
 
 transform = transforms.Compose([
-    transforms.ToTensor()
+    transforms.ToTensor(),
     transforms.Normalize((0.1307,),(0.3081,))
 ])
-train_dataset = dataset.MNIST(
-    root = 
+train_dataset = datasets.MNIST(
+    root = './data',
     train = True,
     download = True,
-    
+    transform = transform
+)
+test_dataset = datasets.MNIST(
+    root = './data',
+    train = True,
+    download = True,
+    transform = transform
+)
+train_loader = DataLoader(
+    train_dataset,
+    batch_size = 64,
+    shuffle = True
+)
+test_loader = DataLoader(
+    test_dataset,
+    batch_size = 64,
+    shuffle = False
 )
