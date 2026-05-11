ac0971c21dc3ab244cad54970f673e10f7c25d14
cjy <1181869833@qq.com>
2026-05-11 12:49:59 +0800

    'eval2'

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,13 @@
 from dataset import test_loader
 from model import MLP 
 import torch
+
+model = MLP()
+model.eval()
+
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for X,y in test_loader：
+        outputs = model(X)
