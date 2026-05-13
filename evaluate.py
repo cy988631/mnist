3b1d77512521d09ffdc89b8770662219a24ea144
cjy <cjy@cjydeMacBook-Air.local>
2026-05-14 01:23:28 +0800

    混淆矩阵1

--- evaluate.py
+++ evaluate.py
@@ -12,9 +12,13 @@ else:
 model = MLP().to(device)
 model.load_state_dict(torch.load('mnist_MLP_model.pth'))
 model.eval()
+torch.no_grad()
 
 correct = 0
 total = 0
+
+all_preds = []
+all_labels = []
 
 with torch.no_grad():
     for X,y in test_loader:
@@ -23,6 +27,10 @@ with torch.no_grad():
         _,predicted = torch.max(outputs,dim = 1)
         total += y.size(0)
         correct += (predicted == y).sum().item()
+
+        predicted_numpy = predicted.cpu().numpy()
+        y_numpy = y.cpu().numpy()
+        
     accuracy = correct/total
     
 print(f'准确率: {100 * accuracy:.2f}%')
